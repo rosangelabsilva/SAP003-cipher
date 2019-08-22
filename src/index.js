7a65115892b8b6780b1eefc0da1043367c860b3f
rosangelabsilva <rosangela-bsilva@hotmail.com>
2019-08-22 17:51:41 -0300

    corrigido erros de pontuação e inicio do css e testes

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 //eventos da página: clique, input
 //todas as interações com HTML
-function clickEncode(){
+function clickEncode() {
   event.preventDefault();
   let mensagem = document.getElementById("mensagem").value;
   let chave = parseInt(document.getElementById("chave").value);
   let result = window.cipher.encode(mensagem, chave);
-  document.getElementById("message").innerHTML= result
+  document.getElementById("message").innerHTML= result;
 }
+clickEncode();
 
-function clickDecode(){
+function clickDecode() {
   event.preventDefault();
   let mensagem = document.getElementById("mensagem").value;
   let chave = parseInt(document.getElementById("chave").value);
   let result1= window.cipher.decode(mensagem, chave);
-  document.getElementById("message2").innerHTML= result1
+  document.getElementById("message2").innerHTML= result1;
 
 }
+clickDecode();
