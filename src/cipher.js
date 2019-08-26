85137b9eef9353b654abb82439419c796b2e74cd
rosangelabsilva <rosangela-bsilva@hotmail.com>
2019-08-26 15:30:19 -0300

    incluido funções para letras minusculas e realizado testes

--- src/cipher.js
+++ src/cipher.js
@@ -4,21 +4,36 @@ window.cipher = {
   decode: decode
 };
 function encode(chave, mensagem) {
+  // cifra a msg e usa o deslocamento
   let resultado="";
+  let encodeMessage="";
   for (let i=0; i<mensagem.length; i++) {
-    let encodeMessage = ((mensagem.charCodeAt(i) - 65 + chave) % 26) + 65;
-    resultado += String.fromCharCode(encodeMessage);
+    if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+      encodeMessage=((mensagem.charCodeAt(i) - 65 + chave) % 26) + 65;
+      resultado += String.fromCharCode(encodeMessage);
+    } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
+      encodeMessage=((mensagem.charCodeAt(i) - 97 + chave) % 26) + 97;
+      resultado += String.fromCharCode(encodeMessage);
+    } else {
+      resultado += String.fromCharCode(mensagem.charCodeAt(i));
+    }
   }
   return resultado;
-//  return mensagem + " " + chave
-  // encoda a msg e usa o deslocamento
 }
 function decode(chave, mensagem) {
-//desencoda a msg com o deslocamento
+//descifra a msg com o deslocamento
   let decifrar="";
+  let decodeMessage="";
   for (let i=0; i<mensagem.length; i++) {
-    let decodeMessage = ((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90;
-    decifrar += String.fromCharCode(decodeMessage);
+    if (mensagem.charCodeAt(i) >= 65 && mensagem.charCodeAt(i) <= 90) {
+      decodeMessage = ((mensagem.charCodeAt(i) - 90 - chave) % 26) + 90;
+      decifrar += String.fromCharCode(decodeMessage);
+    } else if (mensagem.charCodeAt(i) >= 97 && mensagem.charCodeAt(i) <= 122) {
+      decodeMessage = ((mensagem.charCodeAt(i) - 122 - chave) % 26) + 122;
+      decifrar += String.fromCharCode(decodeMessage);
+    } else {
+      decifrar += String.fromCharCode(mensagem.charCodeAt(i));
+    }
   }
   return decifrar;
 }
